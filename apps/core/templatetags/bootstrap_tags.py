9d92fb1570f336c38a7497cccc3ab2090932f850
Julian Perelli <jperelli@gmail.com>
2018-06-29 02:09:28 -0300

    agregando mejores ads

--- apps/core/templatetags/bootstrap_tags.py
+++ apps/core/templatetags/bootstrap_tags.py
@@ -3,8 +3,7 @@ from django.forms.widgets import CheckboxInput, RadioSelect
 from django.template import Context
 from django.template.loader import get_template
 from django import template
-from django.core.paginator import Paginator
-
+from math import ceil
 
 register = template.Library()
 
@@ -40,3 +39,26 @@ def dividir_columnas(lista, cantidad_columnas):
     for i in range(cantidad_columnas):
         result.append(lista[i*tamano:(i+1)*tamano])
     return result
+
+@register.filter
+def partition_horizontal(thelist, n):
+    """
+    Break a list into ``n`` peices, but "horizontally." That is, 
+    ``partition_horizontal(range(10), 3)`` gives::
+
+        [[1, 2, 3],
+         [4, 5, 6],
+         [7, 8, 9],
+         [10]]
+
+    Clear as mud?
+    """
+    try:
+        n = int(n)
+        thelist = list(thelist)
+    except (ValueError, TypeError):
+        return [thelist]
+    newlists = [list() for i in range(int(ceil(len(thelist) / float(n))))]
+    for i, val in enumerate(thelist):
+        newlists[i/n].append(val)
+    return newlists
