2df4159e7d23db27b91465b5723d60d8230c7227
Julian Perelli <jperelli@gmail.com>
2018-10-07 16:39:49 -0300

    mostrar resultados similares para el 404 de poi

--- apps/catastro/views.py
+++ apps/catastro/views.py
@@ -11,15 +11,42 @@ from django.db.models import Prefetch
 from django.contrib.gis.db.models.functions import Distance
 
 
+def fuzzy_like_query(q):
+    params = {"q": q}
+    query = """
+        SELECT
+            id,
+            slug,
+            nom,
+            latlng::bytea
+        FROM
+            catastro_poi
+        WHERE
+            slug %% %(q)s
+        ORDER BY
+            similarity(slug, %(q)s) DESC
+        LIMIT
+            10
+        ;
+    """
+    query_set = Poi.objects.raw(query, params)
+    return list(query_set)
+
+
 @csrf_exempt
 @require_GET
 def poiORint(request, slug=None):
     poi = None
     pois = Poi.objects.filter(slug=slug)
-    if pois:
-        poi = pois[0]
-    else:
-        poi = get_object_or_404(Interseccion, slug=slug)
+    try:
+        if pois:
+            poi = pois[0]
+        else:
+            poi = get_object_or_404(Interseccion, slug=slug)
+    except Exception as e:
+        pois = fuzzy_like_query(slug)
+        slug = slug.replace('-', ' ')
+        return render(request, 'catastro/ver_poi-404.html', {'slug': slug, 'pois': pois}, status=404)
     # TODO: resolver estas queries en 4 threads
     #       ver https://stackoverflow.com/a/12542927/912450
     recorridos = Recorrido.objects \
