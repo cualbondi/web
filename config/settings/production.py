c892cd51f6780f216de2abf273d6df98a141588a
Julian Perelli <jperelli@gmail.com>
2024-06-27 13:32:56 +0200

    upgrade sentry package

--- config/settings/production.py
+++ config/settings/production.py
@@ -145,10 +145,15 @@ CORS_ORIGIN_WHITELIST = [
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+import sentry_sdk
 
-import raven
-
-INSTALLED_APPS += ['raven.contrib.django.raven_compat']  # noqa F405
-RAVEN_CONFIG = {
-    'dsn': env('RAVEN_DSN_WEB', default=''),
-}
+sentry_sdk.init(
+    dsn=env('SENTRY_DSN', default=''),
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    traces_sample_rate=1.0,
+    # Set profiles_sample_rate to 1.0 to profile 100%
+    # of sampled transactions.
+    # We recommend adjusting this value in production.
+    profiles_sample_rate=0.1,
+)
