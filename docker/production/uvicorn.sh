16284f0274ea3d8e2cb1e3d000bbef6766be6710
Julian Perelli <jperelli@gmail.com>
2024-09-03 13:30:38 +0200

    avoid uvicorn warning

--- docker/production/uvicorn.sh
+++ docker/production/uvicorn.sh
@@ -7,4 +7,4 @@ set -o nounset
 
 /venv/bin/python3 /app/manage.py collectstatic --noinput
 /venv/bin/python3 /app/manage.py compilemessages
-/venv/bin/uvicorn config.asgi:application --host 0.0.0.0 --port 8000 --workers 20
+/venv/bin/uvicorn config.asgi:application --lifespan off --host 0.0.0.0 --port 8000 --workers 20
