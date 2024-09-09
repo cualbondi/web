5d8ae5f0d8da1023806b3e25ffe7af757a451780
Julian Perelli <jperelli@gmail.com>
2024-09-10 01:56:07 +0200

    block some bots

--- apps/core/middlewares/log.py
+++ apps/core/middlewares/log.py
@@ -1,9 +1,29 @@
 from django.utils.deprecation import MiddlewareMixin
+from django.core.exceptions import PermissionDenied
 
 
 class LoggingMiddleware(MiddlewareMixin):
     def process_request(self, request):
         path = request.get_full_path()
-        user_agent = request.META.get("HTTP_USER_AGENT")
+        ua = request.META.get("HTTP_USER_AGENT")
         ip = request.META.get("HTTP_X_FORWARDED_FOR") or request.META.get("REMOTE_ADDR")
-        print(ip, user_agent, path)
+        blockuas = [
+            'Bytespider',
+            'DataForSeoBot',
+            'ImagesiftBot',
+            'petalsearch',
+            'SemrushBot',
+            'proximic',
+            'FriendlyCrawler',
+            'opensiteexplorer',
+            'CriteoBot',
+            'Qwantbot',
+            'Amazonbot',
+            # 'meta-externalagent', # facebook for AI
+        ]
+        for blockua in blockuas:
+            if blockua in ua:
+                print('[BLOCK]', ip, ua, path)
+                raise PermissionDenied("Forbidden user agent")
+
+        print('[-> OK]', ip, ua, path)
