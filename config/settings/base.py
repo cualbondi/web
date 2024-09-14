ffd0642ba1e8517405510e54f253aa866d0384c0
Julian Perelli <jperelli@gmail.com>
2024-09-14 14:11:33 +0200

    fix error 500

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,7 +29,7 @@ TIME_ZONE = 'UTC'
 # https://docs.djangoproject.com/en/dev/ref/settings/#language-code
 LANGUAGE_CODE = 'en-us'
 # https://docs.djangoproject.com/en/dev/ref/settings/#site-id
-# SITE_ID = 1
+SITE_ID = 1
 # https://docs.djangoproject.com/en/dev/ref/settings/#use-i18n
 USE_I18N = True
 LOCALE_PATHS = [
